dbe395a18b7ac22e21d23b7010c643798f105358
Emanuele213 <vittorius213@gmail.com>
2022-10-25 10:30:20 +0200

    add color on squares

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const show = document.querySelector('.container');
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 15 === 0) {
         //console.log ('FIZZBUZZ');
-        const element = `<div class="qr">${'FIZZBUZZ'}</div>`;
+        const element = `<div class="qr fb">${'FIZZBUZZ'}</div>`;
         show.innerHTML += element;
     }
     else if (i % 3 === 0) {
         //console.log ('FIZZ');
-        const element = `<div class="qr">${'FIZZ'}</div>`;
+        const element = `<div class="qr fizz">${'FIZZ'}</div>`;
         show.innerHTML += element;
     }
     else if (i % 5 === 0) {
         //console.log ('BUZZ');
-        const element = `<div class="qr">${'BUZZ'}</div>`;
+        const element = `<div class="qr buzz">${'BUZZ'}</div>`;
         show.innerHTML += element;
     }
     else {
         //console.log(i);
-        const element = `<div class="qr">${i}</div>`;
+        const element = `<div class="qr norm">${i}</div>`;
         show.innerHTML += element;
     }
 };
